606eb9772742a0132678197b6673cc58197b172e
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-07-11 13:44:41 +0300

    changed mssql connection for linux container

--- dal/SQLconnection.js
+++ dal/SQLconnection.js
@@ -1,12 +1,13 @@
-const sql = require("mssql/msnodesqlv8");
-//const sql = require('mssql')
+//const sql = require("mssql/msnodesqlv8");
+const sql = require('mssql')
 
 const config = {
-    server: '(localdb)\\SqlExpress',
-    // user: 'sa',
-    // password: 'Ehaviv1978',
-    // server: 'host.docker.internal',
-    driver: 'msnodesqlv8',
+    //server: '(localdb)\\SqlExpress',
+    user: 'sa',
+    password: 'Ehaviv1978',
+    server: 'localhost',
+   // server: 'host.docker.internal',
+   // driver: 'msnodesqlv8',
     database: 'FakeLook',
     options: {
         enableArithAbort: true
